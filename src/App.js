4416c468d35c91f333d8fd42baa69ba7a97f2a82
wsinghakul <w_singhakul@hotmail.com>
2024-02-27 00:18:37 +1100

    clean up code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/Navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
+import Directory from "./components/directory/directory.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        <Route index={true} element={<Home />} />
+        <Route index={true} element={<Directory />} />
         <Route path="shop/*" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
         <Route path="checkout" element={<Checkout />} />
