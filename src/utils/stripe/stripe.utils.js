3c559295b94cd0cd3ab69dc5e34fa15f92edff54
wsinghakul <w_singhakul@hotmail.com>
2024-06-06 11:54:22 +1000

    finished implementing stripe payment

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,3 +1,3 @@
 import { loadStripe } from "@stripe/stripe-js";
 
-export const stripePromise = loadStripe("pk_test_51PNlP1Bhp25ClHcIncsdN0tSyNg0L4fbVZvmjWpmcLqo7moc456d65szwwZsbZxYztteJ6ieWpnBtZPSTn8RfLCy00hgT4oPXd")
+export const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY)
